40064c5adcd1b91aa9cc9da988b5094b6f748ef7
saulobw <saulo2186@gmail.com>
2025-12-05 14:29:19 -0500

    Agregando Bandit SAST y codigo con hash MD5 debil

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 import os
+import hashlib # Importamos librería de hash
 
 def run_app():
-    print("Iniciando aplicacion segura... o tal vez no.")
+    print("Iniciando aplicacion segura...")
 
-    # SOLUCIÓN: Leemos del entorno del sistema, no del archivo.
     AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-    AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 
     if AWS_ACCESS_KEY_ID:
         print(f"Conectando con la llave: {AWS_ACCESS_KEY_ID}")
-    else:
-        print("No se encontró credencial, pero el código es seguro.")
+
+    # VULNERABILIDAD SAST: Uso de MD5 (Inseguro)
+    password = "supersecretpassword"
+    # Bandit detectará que 'md5' es una función prohibida
+    hash_obj = hashlib.md5(password.encode()) 
+    print(f"Hash inseguro: {hash_obj.hexdigest()}")
 
 if __name__ == "__main__":
     run_app()
